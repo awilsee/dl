1c87eefcdc5ea7c09c10749e2f80283f8c79de8b
Andreas, Wilhelm <awilsee@gmail.com>
2018-01-28 11:20:03 +0100

    building model and some pics added

--- code/scripts/retrain.py
+++ code/scripts/retrain.py
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# edited by Andreas Wilhelm and Alice Bollenmiller
+# for the purposes of the lecture Deep Learning
+#
 # ==============================================================================
 r"""Simple transfer learning with Inception v3 or Mobilenet models.
 
